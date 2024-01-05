596835ac8ade3d2df4263ff33aa1ab96dbe79af3
AGOODFROG <caden.j.quirion@gmail.com>
2024-01-05 14:36:53 -0500

    made logic() and winLogic
    logic is a simple case statment using some recurtion just a bit.
    code is nice and readable

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
 
 function getComputerChoice(){
-    let choices = ["rock","paper", "sizzors"]
-    let out = choice[Math.floor(Math.random() * choice.length)]// the code in brackits picks the index 
+    const choice = ["rock","paper", "sizzors"]
+    //let out = choice[Math.floor(Math.random() * choice.length)]// the code in brackits picks the index 
+     let out = "sizzors"
     return out
 
 }
 getComputerChoice()
+
+let computerSelection = getComputerChoice()
+let playerSelection = prompt("test");
+
+function winLogic(winningInupt){
+    if(playerSelection === winningInupt){
+        console.log("you win")
+    }else{
+        console.log("you loose")
+    }
+}
+
+function logic(computerSelection, playerSelection){
+    computerSelection = computerSelection.toLowerCase()
+    switch(computerSelection){
+        case(playerSelection):{
+            console.log("draw")
+            playerSelection = prompt("draw pick again")
+            logic(computerSelection,playerSelection)
+            break
+        }
+        case('rock'):{
+            winLogic("paper")
+            break
+        }
+        case("sizzors"):{
+            winLogic("rock")
+            break
+        }
+        case("paper"):{
+            winLogic("sizzors")
+            break
+        }
+    }
+}
+   
+
+logic(computerSelection,playerSelection)
