9bd003a8d35e3bc76cda997e4dec8bed2095016a
AGOODFROG <caden.j.quirion@gmail.com>
2024-01-05 13:49:52 -0500

    made getComputerChoice() function add script tag
    getComputerChoice() needs to be cleaned up a bit but it is nice

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function getComputerChoice(){
+    let choices = ["rock","paper", "sizzors"]
+    let out = choice[Math.floor(Math.random() * choice.length)]// the code in brackits picks the index 
+    return out
+
+}
+getComputerChoice()
