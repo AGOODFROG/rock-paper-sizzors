aab846d7275c40d07a2cdb6a26337ba667a862f7
AGOODFROG <caden.j.quirion@gmail.com>
2024-02-09 19:20:30 -0500

    added output to user so they know if they won lost
    or drawed, added all console.log to GUI

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 let computerScore = 0
 let playerScore = 0
 
-const playerScoreOutput = document.getElementById("playerScore")
+const playerScoreOutput = document.querySelector("#playerScore")
 const comperterScoreOutput = document.querySelector("#botScore")
 console.log(playerScoreOutput,comperterScoreOutput)
-console.log(`bot:${computerScore}, you:${playerScore}`)
+
 
 const rock = document.querySelector("#rock")
 const paper = document.querySelector("#paper")
 const skizzors = document.querySelector("#skizzors")
+
+const scoreOutput = document.querySelector("#console")
 
 rock.addEventListener("click",()=>{ logic("rock")})
 paper.addEventListener("click",()=>{logic("paper")})
@@ -26,33 +28,39 @@ function winLogic(winningInupt, playerSelection,computerSelection){
     }else if(playerSelection === undefined){
         return ""
     }else if(playerSelection === winningInupt){
-        console.log(`${playerSelection} beets ${computerSelection}`)
-       
+        scoreOutput.textContent = `${playerSelection} beets ${computerSelection}`
+        playerScore ++
+        playerScoreOutput.textContent = playerScore
+        
        return true // win  
     }
     else{
-        console.log(`${playerSelection} looses to ${computerSelection}`)
+        scoreOutput.textContent = `${playerSelection} looses to ${computerSelection}`
+        computerScore ++
+        comperterScoreOutput.textContent = computerScore
         return false
     }
+
 }
 
 function logic(playerSelection){
     let computerSelection = getComputerChoice()
+    console.log(playerScore,computerScore)
     
     
     switch(computerSelection){
         case(playerSelection):{
-            console.log("draw")
+            scoreOutput.textContent ="draw"
             break;
         }
         case('rock'):{
-             winLogic("paper", playerSelection,computerSelection) 
+             return winLogic("paper", playerSelection,computerSelection) 
         }
         case("skizzors"):{
-             winLogic("rock", playerSelection,computerSelection) 
+            return winLogic("rock", playerSelection,computerSelection) 
         }
         case("paper"):{
-            winLogic("skizzors", playerSelection,computerSelection)  
+            return winLogic("skizzors", playerSelection,computerSelection)  
         }
        
     }
